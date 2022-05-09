f6614205bd9dcf916a6ffcde280b3404dcbec2e9
Nafees <abdur17@student.sust.edu>
2022-05-09 22:23:54 +0600

    ifame done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Card, Spinner } from 'react-bootstrap';
+import Iframe from 'react-iframe';
 import { useNavigate } from 'react-router-dom';
 import Banner from '../Banner/Banner';
 import Product from '../Product/Product';
@@ -15,13 +16,11 @@ const Home = () => {
 
   }, [])
 
-
-
   return (
     <div classname="overflow-x-hidden">
       <Banner></Banner>
       <div className=' w-75 mx-auto'>
-        <h2 className='text-center my-10 text-decoration-underline'>Feature Products: {items.length}</h2>
+        <h2 className='text-center my-10 '>Feature Products: {items.length}</h2>
         {items.length !== 0 ? <div></div> : <div className='flex justify-center'>
           <Spinner id='spinner' animation="grow" />
           <Spinner id='spinner' animation="grow" />
@@ -39,6 +38,15 @@ const Home = () => {
 
       <div className='flex justify-center my-5'>
         <Button className='px-5' onClick={() => { navigate('/manageitems') }}>Manage Items </Button>
+      </div>
+
+      <div className='flex flex-col items-center justify-center pb-10 mx-2'>
+        <p className='text-center  fw-bold fs-3  '>Youtube Videos</p>
+        <div className='grid grid-cols-1 xl:grid-cols-2'>
+        <iframe className='p-3'  width="500" height="315" src="https://www.youtube.com/embed/XiAqmlIEFi0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        <iframe className='p-3' width="500" height="315" src="https://www.youtube.com/embed/zyBh1-1bSBA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
       </div>
     </div>
   );
